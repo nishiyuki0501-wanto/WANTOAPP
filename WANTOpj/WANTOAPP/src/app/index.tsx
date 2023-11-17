3cf4f72692abfa20c4a37e4d09aeaa41c9758cb0
nishiyuki0501 <yuki.nishisaka@ed-wanto.com>
2023-11-17 18:57:56 +0900

    after capture

--- WANTOpj/WANTOAPP/src/app/index.tsx
+++ WANTOpj/WANTOAPP/src/app/index.tsx
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native'
 
 const Index = (): JSX.Element => {
-  const [tasks, setTasks] = useState<string[]>([]);
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
 
-  const addTask = () => {
-    const newTask = `Task ${tasks.length + 1}`;
-    setTasks([...tasks, newTask]);
-  };
-
-  return (
-    <View>
-      <Button title="Add Task" onPress={addTask} />
-      {tasks.map((task, index) => (
-        <Text key={index}>{task}</Text>
-      ))}
-    </View>
-  );
-};
-
-export default Index;
+export default Index
