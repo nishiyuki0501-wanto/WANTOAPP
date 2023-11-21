8cd16cdde0fb5767d45e57a9f55d9334887719f6
nishiyuki0501 <yuki.nishisaka@ed-wanto.com>
2023-11-21 18:24:28 +0900

    udemy56 done

--- WANTOpj/WANTOAPP/src/app/index.tsx
+++ WANTOpj/WANTOAPP/src/app/index.tsx
@@ -1,11 +1,58 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style = {styles.container}>
+            <View>
+                <View>
+                    <View>
+                        <Text>WANTO</Text>
+                        <Text>ログアウト</Text>
+                    </View>
+                </View>
+            </View>
+            <View>
+            <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2023/11/21 11:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2023/11/21 11:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2023/11/21 11:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+            </View>
+            <View>
+                <Text>+</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default Index
